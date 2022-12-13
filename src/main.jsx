a7855bdead6cab982794d94120c0e3844e8bb43a
Eric Lehmann <equilehmann92@gmail.com>
2022-12-13 13:09:44 -0300

    Cambio Browser Router por HashRouter (prueba)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
@@ -8,8 +9,8 @@ import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
