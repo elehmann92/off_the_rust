1b9d49063fe4dc6ea5b80c3dc0b69404a052eac0
Eric Lehmann <equilehmann92@gmail.com>
2022-12-14 15:07:35 -0300

    Landing format

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,19 +6,21 @@ import { Typography } from "@mui/material";
 function Landing() {
   const navigate = useNavigate();
   return (
-    <div className="landing-container">
-      <img src='/favicon.png' alt="moving out"/>
-      <Typography>HAY MUDANZA</Typography>
-      <Typography>ERGO SE VENDEN COSITAS</Typography>
-      <Typography>SI TE COPA ALGO, DALE AL BOTONCITO DE WHATSAPP EN EL DETALLE DEL PRODUCTO Y CHARLAMOS 😊</Typography>
-      <Button
-        style={{ backgroundColor: "var(--primaryColor)" }}
-        size="large"
-        variant="contained"
-        onClick={() => navigate("/products")}
-      >
-        A VEEEEER!
-      </Button>
+    <div className="landing-main">
+      <div className="landing-container">
+        <img className="landing-img" src='/favicon.png' alt="moving out"/>
+        <Typography>HAY MUDANZA!!</Typography>
+        <Typography>ERGO, SE VENDEN COSITAS</Typography>
+        <Typography>SI TE COPA ALGO, DALE AL BOTONCITO DE WHATSAPP EN EL DETALLE DEL PRODUCTO Y CHARLAMOS 😊</Typography>
+        <Button
+          style={{ backgroundColor: "var(--primaryColor)" }}
+          size="large"
+          variant="contained"
+          onClick={() => navigate("/products")}
+        >
+          A VEEEEER!
+        </Button>
+      </div>
     </div>
   );
 }
