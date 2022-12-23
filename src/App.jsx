e31335105571c7cf2c3b55e235964a26c73394fe
Eric Lehmann <equilehmann92@gmail.com>
2022-12-23 11:05:34 -0300

    Dash y cambios en productos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import Dash from "./pages/Dash";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/dash" element={<Dash />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Dash.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { products } from "../assets/products/products.json";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "PRODUCTO", width: 250 },
+  { field: "status", headerName: "STATUS", width: 130 },
+  { field: "cobrado", headerName: "PAGO", width: 130 },
+  { field: "comprador", headerName: "COMPRADOR", width: 130 },
+  { field: "price", headerName: "PRECIO", width: 130 },
+];
+
+const rows = products.map((product) => {
+  return {
+    id: product.id,
+    comprador: product.comprador || null,
+    status: product.status,
+    cobrado: product.cobrado || null,
+    price: product.price,
+    name: product.name,
+  };
+});
+
+export default function Dash() {
+  return (
+    <div style={{ height: "90vh", width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
+  );
+}
