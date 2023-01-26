4e443531d2750edf38111e11c8031d4caf7a7d96
Eric Lehmann <equilehmann92@gmail.com>
2023-01-26 10:42:15 -0300

    backtotop button zooms-in but doesn't zoom out

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -1,17 +1,43 @@
 import React from "react";
-import {useState, useEffect} from "react"
+import { useState, useEffect , useRef} from "react";
 
-function BackToTopButton({scrollUp}) {
-  const [animationState, setAnimationState] = useState("")
-  
-  useEffect(()=> {
-    setAnimationState("zoom-in");
-    return () => {setAnimationState("zoom-out")}
-  },[])
-  
+function BackToTopButton({ scrollUp }) {
+  const [animationState, setAnimationState] = useState("zoom-in");
+  const [shouldRender, setShouldRender] = useState(false);
+  const buttonRef = useRef(null)
+
+  useEffect(() => {
+    setShouldRender(true);
+    return () => {
+      setAnimationState("zoom-out");
+      setTimeout(() => {
+        setShouldRender(false);
+      }, 500);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      buttonRef.current.addEventListener("animationend", () => {
+        if (animationState === "zoom-out") {
+          setShouldRender(false);
+        }
+      });
+    }
+  }, [animationState]);
+
+  if (!shouldRender) {
+    return null;
+  }
+
   return (
     <div>
-      <button onClick={scrollUp} className={`back-to-top ${animationState}`} title="Back to Top">
+      <button
+        onClick={scrollUp}
+        className={`back-to-top ${animationState}`}
+        title="Back to Top"
+        ref={buttonRef}
+      >
         <img
           src="/arrow-up.svg"
           alt="Back to Top"
